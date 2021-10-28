5d43571f266c58470fad16e5c12d27fcd1551ecb
hiroki <assdroi555@gmail.com>
2021-10-28 23:28:44 +0900

    feat: 拡張機能ボタン押すと、アラートが出せるように

---
+++ background.js
@@ -0,0 +1,41 @@
+// background.js
+// manifestv3での現在のタブ取得 & js実行
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+let tab = getCurrentTab();
+
+chrome.action.onClicked.addListener((tab) => { // tab? 多分今開いているタブ
+  chrome.scripting.executeScript({ // どんなときに実行される？
+    target: {tabId: tab.id},
+    files: ['content.js']
+  });
+});
+
+// // Regex-pattern to check URLs against. 
+// // It matches URLs like: http[s]://[...]stackoverflow.com[...]
+// var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
+
+// // A function to use as callback
+// function doStuffWithDom(domContent) {
+//   console.log('I received the following DOM content:\n' + domContent);
+// }
+
+// // When the browser-action button is clicked...
+// // chrome.browserAction.onClicked.addListener(function (tab) {
+
+// // });
+
+// chrome.action.onClicked.addListener((tab) => { // tab? 多分今開いているタブ
+//   // chrome.scripting.executeScript({ // jsを実行するぜ
+//   //   target: {tabId: tab.id}, // tabId: 今開いているタブのIDに向けて
+//   //   files: ['content.js'] // content.jsを発動！
+//   // });
+//   // ...check the URL of the active tab against our pattern and...
+//   if (urlRegex.test(tab.url)) {
+//     // ...if it matches, send a message specifying a callback too
+//     chrome.tabs.sendMessage(tab.id, { text: 'report_back' }, doStuffWithDom);
+//   }
+// });
