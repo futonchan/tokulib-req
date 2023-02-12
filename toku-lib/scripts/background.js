9325cd3d445b40bf5c8fd9bd0b023a36c27caa33
hiroki <assdroi555@gmail.com>
2023-02-12 11:02:04 +0900

    feat: #13 拡張機能をインストール後、オプションページを自動で開くように

--- toku-lib/scripts/background.js
+++ toku-lib/scripts/background.js
@@ -20,3 +20,9 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     return true;
   }
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.tabs.create({
+    url: "chrome-extension://" + chrome.runtime.id + "/options.html"
+  });
+});
